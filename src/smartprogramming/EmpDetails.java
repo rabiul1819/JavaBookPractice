0af9218c3e9ffb7dc67eee0a2b02f1038908072b
S M Rabiul <rabiulislam1819@gmail.com>
2025-09-19 23:13:28 +0600

    second

---
+++ src/smartprogramming/EmpDetails.java
@@ -0,0 +1,18 @@
+package smartprogramming;
+
+public class EmpDetails // Empdetails class
+{
+    //This is no argument method
+    EmpDetails(){
+
+    }
+    // //This is  argument method
+    EmpDetails(int a){
+
+    }
+    //This method calculates the manual salary of the employ
+    void calAnnualSal(){
+
+    }
+
+}
