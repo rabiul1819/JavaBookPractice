120f10a3a74eed9fee36fbf401ec3334667f00d5
S M Rabiul <rabiulislam1819@gmail.com>
2025-09-21 12:39:32 +0600

    Pattern2

--- src/lessonOne/pattern/Pattern2.java
+++ src/lessonOne/pattern/Pattern2.java
@@ -2,7 +2,7 @@ package lessonOne.pattern;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String[] args) {
 
         System.out.print("Enter number of rows: ");
@@ -11,7 +11,7 @@ public class Pattern1 {
         int row,col;
         for (row = 1; row <= n; row++){
             for (col = 1; col <= row; col++){
-                System.out.print(col + " ");
+                System.out.print(row + " ");
             }
             System.out.print("\n");
         }
