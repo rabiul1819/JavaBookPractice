8e4c04545f7f42e2b7c6b1f42f2215abfa1d3a6a
S M Rabiul <rabiulislam1819@gmail.com>
2025-09-21 13:43:11 +0600

    pattern7,8,9

---
+++ src/lessonOne/patternsrightangle/Pattern8.java
@@ -0,0 +1,21 @@
+package lessonOne.patternsrightangle;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+
+        System.out.print("Enter number of rows: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int row,col;
+        for (row = 1; row <= n; row++){
+            for (col = 1; col <= row; col++){
+                System.out.print((char) (col + 96) + " ");
+            }
+            System.out.print("\n");
+        }
+
+        scanner.close();
+    }
+}
